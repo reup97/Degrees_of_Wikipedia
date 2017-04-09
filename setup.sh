a074fdd96340bd1584a2cc453b8bb7b6a3d8749f
Paul Liu <puer@ualberta.ca>
2017-04-09 02:21:19 -0600

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# install dependencies and enveronment.
+echo install bs4
+pip install --upgrade pip
+pip install bs4
+
+echo creating graph_bank directory
+mkdir graph_bank
+
+echo Done.
